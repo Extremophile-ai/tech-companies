3d55f31345ec7c3389d4b811c2f22bb144ed1cba
Uche <godspowercuche56@gmail.com>
2020-08-28 03:20:23 +0100

    implemented try and catch

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -9,66 +9,143 @@ import {
 } from '../query/index';
 
 const getAllNgTechCompanies = (req, res) => {
-  pool.query(
-    getAll,
-    (err, results) => {
-      res.status(200).json(results.rows);
-    },
-  );
+  try {
+    pool.query(
+      getAll,
+      (err, results) => {
+        if (err) {
+          return res.status(400).json({
+            status: 'error',
+            data: err.stack,
+            message: 'Error. Could not get all companies'
+          });
+        } else {
+          return res.status(200).json({
+            status: 'success',
+            message: 'All companies gotten',
+            data: results.rows
+          });
+        }
+      }
+    );
+  } catch (error) {
+    return res.status(500).json({
+      status: 'error',
+      message: error.message
+    });
+  }
 };
 
 const getNgTechCompanyById = (req, res) => {
-  const id = parseInt(req.params.id);
-  pool.query(
-    getOne,
-    [id],
-    (err, results) => {
-      res.status(200).json(results.rows);
-      if (err) {
-        throw err;
-      }
-    },
-  );
+  try {
+    const id = parseInt(req.params.id);
+    pool.query(
+      getOne,
+      [id],
+      (err, results) => {
+        if (err) {
+          throw res.status(404).json({
+            status: 'error',
+            message: 'Oops! An error occured while getting a single company',
+            data: err.stack
+          });
+        } else {
+          res.status(200).json({
+            status: 'success',
+            message: 'One company gottn',
+            data: results.rows
+          });
+        }
+      },
+    );
+  } catch (error) {
+    return res.status(500).json({
+      status: 'error',
+      message: error.message
+    });
+  }
 };
 
 const createNewTechCompany = (req, res) => {
-  const {
-    name, location, name_of_ceo, year_founded, email, website,
-  } = req.body;
-  const Values = [name, location, name_of_ceo, year_founded, website, email];
-  pool.query(createNewText, Values, (err, results) => {
-    if (err) {
-      return err.stack;
-    } else {
-      res.status(200).json(results.rows[0]);
-    }
-  });
+  try {
+    const { name, location, name_of_ceo, year_founded, email, website } = req.body;
+    const Values = [name, location, name_of_ceo, year_founded, email, website];
+    pool.query(createNewText, Values, (err, results) => {
+      if (err) {
+        return res.status(400).json({
+          status: 'error',
+          data: err.stack,
+          message: 'Oops! An error occurred while creating a company.'
+        });
+      } else {
+        return res.status(200).json({
+          status: 'success',
+          message: 'created company successfully',
+          data: results.rows[0],
+        });
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 'error',
+      message: error.message
+    });
+  }
 };
 
 const updateTechCompanyInfo = (req, res) => {
-  const id = parseInt(req.params.id);
-  const {
-    name, location, name_of_ceo, year_founded, email, website,
-  } = req.body;
-  const Values = [name, location, name_of_ceo, year_founded, website, email];
-  pool.query(updateOne, Values,
-    (err, res) => {
-      if (err) {
-        throw err;
-      } else {
-        res.status(200).send(`Company Info modified with ID: ${id}`);
-      }
+  try {
+    const id = parseInt(req.params.id);
+    const { name, location, name_of_ceo, year_founded, email, website } = req.body;
+    const Values = [name, location, name_of_ceo, year_founded, website, email];
+    pool.query(updateOne, Values,
+      (err, res) => {
+        if (err) {
+          return res.status(404).json({
+            status: 'error',
+            message: 'Error! Company not found',
+            data: err.stack
+          });
+        } else {
+          return res.status(200).json({
+            status: 'success',
+            message: `Company Info modified with ID: ${id}`,
+            data: results.rows
+          });
+        }
+      });
+  } catch (error) {
+    return res.status(500).json({
+      status: 'error',
+      message: error.message
     });
+  }
 };
 
 const deleteTechCompany = (req, res) => {
-  const id = parseInt(req.params.id);
-  pool.query(deleteQuery, [id], (error, results) => {
-    if (error) {
-      throw error;
-    }
-    res.status(200).send(`Company deleted with ID: ${id}`);
-  });
+  try {
+    const id = parseInt(req.params.id);
+    pool.query(deleteQuery, [id], (error, results) => {
+      if (error) {
+        throw res.status(400).json({
+          status: 'error',
+          message: 'Error! Company not found',
+          data: err.stack
+        });
+      } else {
+        return res.status(200).json({
+          status: 'success',
+          message: `Company deleted with ID: ${id}`,
+          data: results.rows
+        });
+      }
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: 'error',
+      message: error.message
+    });
+  }
 };
 
 export {
--- src/test/test.js
+++ src/test/test.js
@@ -1,21 +1,40 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../server';
-// import controllers from '../controllers/controllers';
+import {
+  getAllNgTechCompanies,
+  getNgTechCompanyById,
+  createNewTechCompany,
+  updateTechCompanyInfo,
+  deleteTechCompany,
+} from '../controllers/controllers';
 
 // assertion style
-should = chai.should();
+chai.should();
 chai.use(chaiHttp);
+const url = '/company';
 
-describe('Server.js should return all endpoints', () => {
-  describe('Get /company', () => {
-    it('it should get all companies', (done) => {
-      const res = chai
+describe('Server.js should return all endpoints', async () => {
+  describe('Test for Create a company', () => {
+    it('it should create a company successfully', async () => {
+      const res = await chai
         .request(server)
-        .get('/company');
-      res.should.have.status(200);
-      (res.body).should.be.a('object');
-      done();
+        .post(`${url}/create`)
+        .send(createNewTechCompany);
+      (res).should.have.status(200);
+      // (res).should.be.a('string');
+      // done();
     });
   });
+
+  // describe('Get /company', () => {
+  //   it('it should get all companies', (done) => {
+  //     const res = chai
+  //       .request(server)
+  //       .get('/company');
+  //     res.should.have.status(200);
+  //     (res.body).should.be.a('object');
+  //     done();
+  //   });
+  // });
 });
